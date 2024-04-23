558ebf721b7d7e606603238306234b8493c458ae
Alexander Castillo <acastillo@aviamltd.com>
2024-04-23 10:59:52 -0400

    Add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import React from 'react'
 import Mainlayout from './pages/Mainlayout'
+import SignInSide from './pages/Login'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Mainlayout />
+    // <Mainlayout />
+    <SignInSide />
+
   )
 }
 
